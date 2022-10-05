230fcf37a55c440be7ab28934294468d59d9b9c5
Snowplou <Snowplou01@gmail.com>
2022-10-05 08:59:01 -0700

    high score is now saved locally, it can now detect if user input is right or wrong and if it should add a new color

--- index.js
+++ index.js
@@ -1,38 +1,81 @@
 let possibleColors = ["red", "blue", "orange", "green"]
 let chosenColors = []
 let userColors = []
+let showingColors = false
+let score = 0
+let highScore = localStorage.getItem("highScore")
 
-function red(){
-    console.log("red was clicked")
+if(highScore == null){
+    highScore = 0
 }
 
-function blue(){
-    console.log("blue was clicked")
+document.getElementById("highScore").innerHTML = "<i><u>High Score</i></u><br>" + highScore
+
+function displayColors(){
+
 }
 
-function orange(){
-    console.log("orange was clicked")
+function turn(color) {
+    if (showingColors) return
+    userColors.push(color)
+    if(userColors[userColors.length - 1] != chosenColors[userColors.length - 1]){
+        document.getElementById("page2").style.display = "none"
+        document.getElementById("page3").style.display = "block"
+        if(score > highScore){
+            localStorage.setItem("highScore", score)
+            highScore = score;
+        }
+        document.getElementById("finalScore").innerHTML = "Score: " + score
+        document.getElementById("finalHighScore").innerHTML = "High Score: " + highScore
+    }
+    else if(userColors.length == chosenColors.length){
+        score++
+        document.getElementById("score").innerHTML = "<i><u>Score</i></u><br>" + score
+        console.log("new turn")
+    }
+    else{
+        score++
+        document.getElementById("score").innerHTML = "<i><u>Score</i></u><br>" + score
+    }
 }
 
-function green(){
-    console.log("green was clicked")
+function red() {
+    turn("red")
 }
 
-function startGame(){
+function blue() {
+    turn("blue")
+}
+
+function orange() {
+    turn("orange")
+}
+
+function green() {
+    turn("green")
+}
+
+function startGame() {
     document.getElementById("page1").style.display = "none"
     document.getElementById("page2").style.display = "block"
 
+    showingColors = true
+
     chosenColors.push(possibleColors[Math.floor(Math.random() * possibleColors.length)])
 
-    let time = 1000
+    chosenColors.push(possibleColors[Math.floor(Math.random() * possibleColors.length)])
+    chosenColors.push(possibleColors[Math.floor(Math.random() * possibleColors.length)])
+
+    // let time = 1500
+    let time = 500
 
     setTimeout(() => {
 
-        for(let i = 0; i < possibleColors.length; i++){
+        for (let i = 0; i < possibleColors.length; i++) {
             document.getElementById(possibleColors[i]).classList.remove("hoverAbility")
         }
 
-        for(let item in chosenColors){
+        for (let item in chosenColors) {
             setTimeout(() => {
 
                 console.log(chosenColors[item])
@@ -41,15 +84,18 @@ function startGame(){
 
                 setTimeout(() => {
                     document.getElementById(chosenColors[item]).style.backgroundColor = chosenColors[item]
-                }, time)
+                }, time * 0.5)
 
             }, time * item)
         }
 
-        for(let i = 0; i < possibleColors.length; i++){
+    }, 1000)
+
+    setTimeout(() => {
+        for (let i = 0; i < possibleColors.length; i++) {
             document.getElementById(possibleColors[i]).classList.add("hoverAbility")
         }
-
-    }, 1000)
+        showingColors = false
+    }, (chosenColors.length * time) + (time * 0.5))
 
 }
